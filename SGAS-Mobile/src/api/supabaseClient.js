afffd125d0e12461d6c8c60c88d377e7308b7645
Berkan <114605289+XlBERKANlX@users.noreply.github.com>
2025-10-15 16:01:12 +0300

    Update supabaseClient.js

--- SGAS-Mobile/src/api/supabaseClient.js
+++ SGAS-Mobile/src/api/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://alooejwhubqlytkmryfr.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsb29landodWJxbHl0a21yeWZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5MzI5MjIsImV4cCI6MjA1OTUwODkyMn0.ID1VfjvkXA9Oh68QJCKoOfgLCGwXhPCcANE_ci5Hyd0';
+const SUPABASE_URL = '.supabase.co';
+const SUPABASE_ANON_KEY = '';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
